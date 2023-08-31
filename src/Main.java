f6759d3f327af664d5fe340307ad8bf2ce8dfff3
iojuch <petrovich8384@mail.ru>
2023-08-31 23:33:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int amount = 13676;
+        int bonus = 20;
+        int mile = amount / bonus;
+        System.out.println("Мили с покупки:" + mile);
+
+    }
+}
